535a2f266ca1619fbe682da2474de42b0df13a25
shubhamsinhadev <147917207+shubhamsinhadev@users.noreply.github.com>
2024-02-22 10:29:31 +0530

    Update Chat.jsx

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 export default function Chat({ socket, username }) {
@@ -6,14 +6,13 @@ export default function Chat({ socket, username }) {
     const [userId, setUserId] = useState('')
     const [messages, setMessages] = useState([]);
     const [inputValue, setInputValue] = useState('');
+    const ref = useRef(null)
 
     useEffect(() => {
         if (!username) {
             navigate('/')
         }
     }, [navigate, username])
-
-
 
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
@@ -34,6 +33,24 @@ export default function Chat({ socket, username }) {
     };
 
     useEffect(() => {
+        const k = ref.current
+        const observer = new MutationObserver((mutationsList) => {
+            for (const mutation of mutationsList) {
+                if (mutation.type === "childList") {
+                    k.scrollTop = k.scrollHeight - k.clientHeight
+                }
+            }
+        });
+
+        observer.observe(k, { attributes: true, childList: true, subtree: true });
+        return () => {
+            observer.disconnect();
+        };
+    }, [inputValue]);
+
+
+
+    useEffect(() => {
         const handleIncomingMessage = (msg) => {
             setMessages((prevMessages) => [...prevMessages, msg]);
         };
@@ -41,16 +58,23 @@ export default function Chat({ socket, username }) {
             setUserId(socket.id)
             setMessages((prevMessages) => [...prevMessages, msg]);
         });
-
         return () => {
             socket.off('chat message', handleIncomingMessage);
         };
     }, [socket]);
+
+
     return (
-        <div className='h-[calc(100vh-50px)] bg-slate-300'>
-            <div className=" mt-[50px] w-full flex justify-center ">
-                <div className="w-[90%] mt-[80px] max-w-[600px] h-[calc(100vh-200px)] bg-white flex flex-col  items-center rounded-md justify-between shadow-md">
-                    <ul className='w-[100%] flex flex-col p-2 gap-2'>
+        <div
+            className='h-[calc(100vh-50px)] bg-slate-300'>
+            <div
+                className=" mt-[50px] w-full flex justify-center ">
+                <div
+                    className="w-[90%] mt-[80px] max-w-[600px] h-[calc(100vh-200px)] bg-white flex flex-col 
+                 items-center rounded-md justify-between shadow-md">
+                    <ul
+                        ref={ref}
+                        className='w-[100%] flex-1 flex flex-col p-2 gap-2 overflow-scroll overflow-x-hidden'>
                         {messages.map((msg, index) => {
                             const { id, text, username } = msg;
                             if (id === userId) {
@@ -84,9 +108,17 @@ export default function Chat({ socket, username }) {
                             )
                         })}
                     </ul>
-                    <form onSubmit={handleSubmit} className=" w-[90%] flex h-[40px] mb-2">
-                        <input type="text" value={inputValue} className=' border-2 border-blue-400 flex-1 rounded-l-md pl-2' onChange={handleInputChange} />
-                        <button type="submit" className=' bg-blue-400 px-4 text-white rounded-r-md hover:bg-blue-600 active:bg-blue-400  font-bold'>Send</button>
+                    <form onSubmit={handleSubmit} className=" w-[90%] flex h-[50px] mb-2">
+                        <input
+                            type="text"
+                            value={inputValue}
+                            className=' border-2 border-blue-400 flex-1 rounded-l-md pl-2'
+                            onChange={handleInputChange} />
+                        <button
+                            type="submit"
+                            className=' bg-blue-400 px-4 text-white rounded-r-md hover:bg-blue-600 active:bg-blue-400  
+                            font-bold'
+                        >Send</button>
                     </form>
                 </div>
             </div>
